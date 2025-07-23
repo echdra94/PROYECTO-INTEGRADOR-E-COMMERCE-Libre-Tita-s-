8947f8064783fd95d073c5e94e072526cbbaeaed
Natalia EO <nataliaeo1445@gmail.com>
2025-07-23 17:04:07 -0600

    Se añade la carpeta de JS junto al archivo main.js

---
+++ JS/JS/main.js
@@ -0,0 +1,19 @@
+// Control de flujo 
+
+// if-else (condiciones)
+
+//** 
+// Cómo crear una condición (expresiones lógicas)
+//  */
+
+let n = 10;
+
+if (n == 10 || n == 9){
+    console.log(`El número de ${n} es igual a 10`);
+    console.log("Se cumple");
+    // Se ejecuta si la condición se cumple
+} else {
+    //Se ejecuta si la confición no se cumple
+    console.logt(`El número de ${n} no es igual a 10`);
+    console.log("No se cumple");
+}
