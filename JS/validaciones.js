f93098612ea19017eb1c2d2bfd53c964af544627
Anya Mares <anyavanessamares@gmail.com>
2025-08-05 15:35:37 -0600

    se agregaron las validaciones de telefono y puse de nuevo cambios html

--- JS/validaciones.js
+++ JS/validaciones.js
@@ -5,6 +5,7 @@ const txtEmpresa = document.getElementById("txtEmpresa");
 const iptCorreo1 = document.getElementById("iptCorreo1"); // iptCorreo1 Correo electrónico
 const txtEspecificaciones = document.getElementById("Textarea1"); // Textarea de especificaciones del proyecto
 const contadorCaracteres = document.getElementById("contadorCaracteres"); // Texto que muestra el conteo de caracteres
+const txtTelefono1 = document.getElementById("txtTelefono1");
 
 btnEnviar.addEventListener("click", function (event){
     event.preventDefault();
@@ -13,6 +14,8 @@ btnEnviar.addEventListener("click", function (event){
     alertValidaciones.style.display="none"; // Limpia el fondo de la alerta
     txtEmpresa.style.boxShadow= "none";
     txtEmpresa.style.border="";// limpia el borde rojo del campo name cuando hay alerta
+    txtTelefono1.style.boxShadow= "none";
+    txtTelefono1.style.border="";
     iptCorreo1.style.boxShadow= "none";
     iptCorreo1.style.border="";// Limpia el borde rojo del campo correo 
     txtEspecificaciones.style.border = ""; // Limpia el borde del campo especificaciones
@@ -27,6 +30,22 @@ btnEnviar.addEventListener("click", function (event){
         isValid = false;
     }
 
+    //Validacion del campo de Telefono 
+    if(txtTelefono1.value.trim() ==="") {
+        txtTelefono1.style.border="thin solid #DD0069";
+        txtTelefono1.style.boxShadow= "0 0 6px 3px rgba(221, 0, 107, 0.6)";
+        alertValidacionesTexto.innerHTML="<strong>Ingresa un número de Teléfono</strong><br>"; // Alerta
+        alertValidaciones.style.display="block"; // Fondo de alerta del div
+        isValid = false;
+    }
+    if(!/^\d{10}$/.test(txtTelefono1.value)){
+    txtTelefono1.style.border="thin solid #DD0069";
+        txtTelefono1.style.boxShadow= "0 0 6px 3px rgba(221, 0, 107, 0.6)";
+        alertValidacionesTexto.innerHTML="<strong>El número no contiene 10 dígitos</strong><br><strong>Este espacio sólo acepta números</strong><br/>"; // Alerta
+        alertValidaciones.style.display="block"; // Fondo de alerta del div
+        isValid = false;
+    }
+
     // Validación del campo Correo electrónico
     const regexCorreo = /^[^\s@]+@[^\s@]+\.[a-zA-Z.]*[a-zA-Z]{2,3}$/;
     if (!regexCorreo.test(iptCorreo1.value)) {
@@ -37,11 +56,13 @@ btnEnviar.addEventListener("click", function (event){
         isValid = false;
     }
 
+    
+
 
     // para validar campo Especificaciones
     const especificacionesTexto = txtEspecificaciones.value.trim(); // Quitamos espacios sobrantes
 
-    if (especificacionesTexto.length < 10 || especificacionesTexto.toLowerCase() === "idea") {
+    if (especificacionesTexto.length < 10) {
         // Si tiene menos de 10 caracteres reales o menos de 10 palabras
         txtEspecificaciones.style.setProperty("border", "1px solid #DD0069", "important");
         txtEspecificaciones.style.boxShadow = "0 0 6px 3px rgba(221, 0, 107, 0.6)";
@@ -60,6 +81,7 @@ btnEnviar.addEventListener("click", function (event){
      if(isValid){
         txtEmpresa.value="";// Limpia el valor de txtName
         txtEmpresa.focus(); // Coloca el cursor en la casilla txtName;
+        txtTelefono1.value="";
         iptCorreo1.value=""; //Limpia el valor de iptCorreo1
         txtEspecificaciones.value = "";
      }
