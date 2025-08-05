8c0c68e3639ae155880455c3557da7e82537d1d8
Natalia EO <nataliaeo1445@gmail.com>
2025-08-05 14:16:16 -0600

    Se agrega la característica de validación de correo electrónico

--- JS/validaciones.js
+++ JS/validaciones.js
@@ -2,6 +2,7 @@ const btnEnviar = document.getElementById("btnEnviar");
 const alertValidaciones = document.getElementById("alertValidaciones");
 const alertValidacionesTexto = document.getElementById("alertValidacionesTexto");
 const txtEmpresa = document.getElementById("txtEmpresa");
+const iptCorreo1 = document.getElementById("iptCorreo1"); // iptCorreo1 Correo electrónico
 const txtEspecificaciones = document.getElementById("Textarea1"); // Textarea de especificaciones del proyecto
 const contadorCaracteres = document.getElementById("contadorCaracteres"); // Texto que muestra el conteo de caracteres
 
@@ -12,6 +13,8 @@ btnEnviar.addEventListener("click", function (event){
     alertValidaciones.style.display="none"; // Limpia el fondo de la alerta
     txtEmpresa.style.boxShadow= "none";
     txtEmpresa.style.border="";// limpia el borde rojo del campo name cuando hay alerta
+    iptCorreo1.style.boxShadow= "none";
+    iptCorreo1.style.border="";// Limpia el borde rojo del campo correo 
     txtEspecificaciones.style.border = ""; // Limpia el borde del campo especificaciones
     txtEspecificaciones.style.boxShadow = ""; // Limpia sombraMargen rojo del campo especificaciones
 
@@ -23,6 +26,17 @@ btnEnviar.addEventListener("click", function (event){
         alertValidaciones.style.display="block"; // Fondo de alerta del div
         isValid = false;
     }
+
+    // Validación del campo Correo electrónico
+    const regexCorreo = /^[^\s@]+@[^\s@]+\.[a-zA-Z.]*[a-zA-Z]{2,3}$/;
+    if (!regexCorreo.test(iptCorreo1.value)) {
+        iptCorreo1.style.border = "thin solid #DD0069";
+        iptCorreo1.style.boxShadow = "0 0 6px 3px rgba(221, 0, 107, 0.6)";
+        alertValidacionesTexto.innerHTML += "<strong>Ingresa un correo electrónico válido</strong><br>";
+        alertValidaciones.style.display = "block";
+        isValid = false;
+    }
+
 
     // para validar campo Especificaciones
     const especificacionesTexto = txtEspecificaciones.value.trim(); // Quitamos espacios sobrantes
@@ -46,12 +60,13 @@ btnEnviar.addEventListener("click", function (event){
      if(isValid){
         txtEmpresa.value="";// Limpia el valor de txtName
         txtEmpresa.focus(); // Coloca el cursor en la casilla txtName;
+        iptCorreo1.value=""; //Limpia el valor de iptCorreo1
         txtEspecificaciones.value = "";
      }
 }
 );
 
-txtEspecificaciones.addEventListener("input", function () {
+    txtEspecificaciones.addEventListener("input", function () {
     let caracteresActuales = txtEspecificaciones.value.length; // Contar cuántos lleva
     contadorCaracteres.innerText = `${caracteresActuales}/500`; // Actualizar el texto
 });
